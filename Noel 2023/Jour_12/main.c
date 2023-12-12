ea6c2e9582847b14f9ad94f6c386056ea856335f
stephdaniel133 <stephdaniel133@hotmail.com>
2023-12-12 07:57:19 +0100

    2023_Jour_12

--- Noel 2023/Jour_12/main.c
+++ Noel 2023/Jour_12/main.c
@@ -9,27 +9,29 @@
 #define max(a,b) ({ __typeof__ (a) _a = (a); __typeof__ (b) _b = (b); _a > _b ? _a : _b; })
 #define min(a,b) ({ __typeof__ (a) _a = (a); __typeof__ (b) _b = (b); _a < _b ? _a : _b; })
 
-//#define NBR_LINES 10
-#define NBR_LINES 140
+#define NBR_LINES 6
+//#define NBR_LINES 1000
 
 //#define NBR_COL 10
-#define NBR_COL 140
+//#define NBR_COL 140
 
 
 int main(int argc, char *argv[])
 {
-    char buf[400];
+    //char buf[400];
     long long i = 0;
     long long j = 0;
     //long long k = 0;
-    char tab[NBR_LINES][NBR_COL];
-
+    char temp;
+    char tab[NBR_LINES][100];
+    int seq[NBR_LINES][10];
     long long result1 = 0;
     long long result2 = 0;
     FILE *fic = NULL;
 
 
     memset(tab, 0, sizeof(tab));
+    memset(seq, 0, sizeof(seq));
 
 
     fic = fopen("data.txt", "r");
@@ -41,8 +43,16 @@ int main(int argc, char *argv[])
         //On lit les tableaux dans le fichier d'entrée
         while(!feof(fic))
         {
-            for(i=0 ; i<NBR_LINES ; i++)
+            //for(i=0 ; i<NBR_LINES ; i++)
                 fscanf(fic, "%s", tab[i]);
+                for(j=0 ; j<10 ; j++)
+                {
+                    fscanf(fic, "%d", &seq[i][j]);
+                    fscanf(fic, "%c", &temp);
+                }
+
+
+                i++;
         }
 
 
