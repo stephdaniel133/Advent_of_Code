ed9f15f14dc67e37857d9a53674ffdc909d35e37
stephdaniel133 <stephdaniel133@hotmail.com>
2024-12-21 20:32:34 +0100

    Jour_19 : optimisation

--- Noel 2024/Jour_19/main.c
+++ Noel 2024/Jour_19/main.c
@@ -7,7 +7,7 @@
 #include <time.h>
 
 
-bool recur(char design[], int len, int owenNbr, char owen[][20], bool result, char owenTest[])
+/*bool recur(char design[], int len, int towelNbr, char towel[][11], bool result, char towelTest[])
 {
     int lenTest = 0;
 
@@ -15,25 +15,79 @@ bool recur(char design[], int len, int owenNbr, char owen[][20], bool result, ch
         return true;
     else
     {
-        char test[100];
+        char test[90];
 
-        for(int j=0 ; j<owenNbr ; j++)
+        for(int j=0 ; j<towelNbr ; j++)
         {
             memset(test, '\0', sizeof(test));
-            strcat(test, owenTest);
-            strcat(test, owen[j]);
-            //printf("%s : %s\n", design, test);
+            strcpy(test, towelTest);
+            strcat(test, towel[j]);
+            printf("%s : %s\n", design, test);
             lenTest = strlen(test);
             if(lenTest<len)
             {
                 if(strncmp(design, test, lenTest) == 0)
                 {
-                    if(true == recur(design, len, owenNbr, owen, result, test))
+                    if(true == recur(design, len, towelNbr, towel, result, test))
                         return true;
                 }
             }
-            else if((lenTest == len) && (strcmp(design, test) == 0))
+            else if((lenTest == len) && (strncmp(design, test, lenTest) == 0))
             {
+                    return true;
+            }
+        }
+
+        return false;
+    }
+}*/
+
+
+/*bool recur(char design[], int len, int towelNbr, char towel[][11], bool result)
+{
+    int lenTest = 0;
+
+    if(result == true)
+        return true;
+    else
+    {
+        //char test[90];
+
+        for(int j=0 ; j<towelNbr ; j++)
+        {
+            //printf("%s : %s\n", design, towel[j]);
+            lenTest = strlen(towel[j]);
+            if(lenTest<len)
+            {
+                if(strncmp(design, towel[j], lenTest) == 0)
+                {
+                    if(true == recur(design+lenTest, strlen(design+lenTest), towelNbr, towel, result))
+                        return true;
+                }
+            }
+            else if((lenTest == len) && (strncmp(design, towel[j], lenTest) == 0))
+            {
+                    return true;
+            }
+        }
+
+        return false;
+    }
+}*/
+
+
+
+bool recur(char design[], int towelNbr, char towel[][11])
+{
+    if(strlen(design) == 0)
+        return true;
+    else
+    {
+        for(int j=0 ; j<towelNbr ; j++)
+        {
+            if(strncmp(design, towel[j], strlen(towel[j])) == 0)
+            {
+                if(true == recur(design+strlen(towel[j]), towelNbr, towel))
                     return true;
             }
         }
@@ -49,17 +103,17 @@ int main(int argc, char *argv[])
     int64_t somme1 = 0;
     int64_t somme2 = 0;
     char buf[1000];
-    char owen[450][20];
-    char design[450][70];
+    char towel[450][11];
+    char design[410][70];
     bool designPossible[450];
     int i = 0;
     int len = 0;
-    int owenNbr = 0;
+    int towelNbr = 0;
     int designNbr = 0;
 
 
     memset(buf, '\0', sizeof(buf));
-    memset(owen, '\0', sizeof(owen));
+    memset(towel, '\0', sizeof(towel));
     memset(design, '\0', sizeof(design));
     memset(designPossible, false, sizeof(designPossible));
 
@@ -72,24 +126,24 @@ int main(int argc, char *argv[])
     {
         printf("fichier ouvert\n");
 
-        //On recupère les owen
+        //On recupère les towel
         i = 0;
         do
         {
             memset(buf, 0, sizeof(buf));
             fscanf(fic, "%s, ", buf);
             if(buf[strlen(buf)-1] == ',')
-                strncpy(&owen[i][0], buf, strlen(buf)-1);
+                strncpy(&towel[i][0], buf, strlen(buf)-1);
             else
-                strncpy(&owen[i][0], buf, strlen(buf));
+                strncpy(&towel[i][0], buf, strlen(buf));
 
             len = strlen(buf);
-            printf("longueur owen %d = %d\n", i, len);
+            printf("longueur towel %d = %d\n", i, len);
             i++;
         }
         while(buf[len-1] == ',');
 
-        owenNbr = i;
+        towelNbr = i;
 
         //On recupère les design
         i = 0;
@@ -98,9 +152,9 @@ int main(int argc, char *argv[])
             memset(buf, 0, sizeof(buf));
             fscanf(fic, "%s\n", buf);
             strcpy(&design[i][0], buf);
-            i++;
             len = strlen(buf);
-            printf("longueur design = %d\n", len);
+            printf("longueur design %d = %d\n", i, len);
+            i++;
         }
         while(!feof(fic));
 
@@ -113,13 +167,15 @@ int main(int argc, char *argv[])
     fclose(fic);
     fic = NULL;
 
-    printf("\n%s\n", design[designNbr-1]);
-    printf("\n%s\n", owen[owenNbr-1]);
+    //printf("\n%s\n", design[designNbr-1]);
+    //printf("\n%s\n", towel[towelNbr-1]);
 
 
     for(i=0 ; i<designNbr ; i++)
     {
-        designPossible[i] = recur(design[i], strlen(design[i]), owenNbr, &owen[0], false, "\0");
+        //designPossible[i] = recur(design[i], strlen(design[i]), towelNbr, &towel[0], false, "\0");
+        //designPossible[i] = recur(design[i], strlen(design[i]), towelNbr, &towel[0], false);
+        designPossible[i] = recur(design[i], towelNbr, &towel[0]);
         printf("Design number %d, %d\n", i, designPossible[i]);
     }
 
