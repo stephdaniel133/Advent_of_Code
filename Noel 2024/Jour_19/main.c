dcbfccd2447eacf9e00de02b4540b18c5097e0be
stephdaniel133 <stephdaniel133@hotmail.com>
2025-01-10 17:52:41 +0100

    Jour_19

--- Noel 2024/Jour_19/main.c
+++ Noel 2024/Jour_19/main.c
@@ -76,23 +76,119 @@
 }*/
 
 
+bool SearchToList(char* wordToSearch, char List[][70])
+{
+    for(size_t i = 0; i<1000 ; i++)
+    {
+        if(List[i][0] == '\0')
+            break;
+        if(!strcmp(wordToSearch, List[i]))
+            return true;
+    }
 
-bool recur(char design[], int towelNbr, char towel[][11])
+    return false;
+}
+
+
+void AddToList(char* wordToAdd, char List[][70])
+{
+    size_t i = 0;
+
+    if(SearchToList(wordToAdd, List))   //Si le mot est déjà dans la liste, on ne l'ajoute pas
+        return;
+
+    for(i = 0; i<1000 ; i++)
+    {
+        if(List[i][0] == '\0')
+            break;
+    }
+    strcpy(List[i], wordToAdd);
+}
+
+
+bool recur(char design[], int towelNbr, char towel[][11], char List[][70])
 {
     if(strlen(design) == 0)
         return true;
     else
     {
+        if(SearchToList(design, List))   //Si le mot est déjà dans la liste, c'est qu'on l'a déjà cherché
+            return false;
+
         for(int j=0 ; j<towelNbr ; j++)
         {
             if(strncmp(design, towel[j], strlen(towel[j])) == 0)
             {
-                if(true == recur(design+strlen(towel[j]), towelNbr, towel))
+                if(true == recur(design+strlen(towel[j]), towelNbr, towel, List))
                     return true;
             }
         }
 
+        AddToList(design, List);
         return false;
+    }
+}
+
+
+/*bool recur2(char design[], int towelNbr, char towel[][11], char List[][70], uint32_t *designPossibleNbr)
+{
+    if(strlen(design) == 0)
+    {
+        (*designPossibleNbr)++;
+        return true;
+    }
+    else
+    {
+        if(SearchToList(design, List))   //Si le mot est déjà dans la liste, c'est qu'on l'a déjà cherché
+            return false;
+
+        for(int j=0 ; j<towelNbr ; j++)
+        {
+            if(strncmp(design, towel[j], strlen(towel[j])) == 0)
+            {
+                if(true == recur2(design+strlen(towel[j]), towelNbr, towel, List, designPossibleNbr))
+                {
+                    //(*designPossibleNbr)++;
+                    //return true;
+                }
+            }
+        }
+
+        //AddToList(design, List);
+        return false;
+    }
+}*/
+
+
+uint32_t recur2(char design[], int towelNbr, char towel[][11], char List[][70])
+{
+    uint32_t trouve = 0;
+
+    if(strlen(design) == 0)
+    {
+        //printf("trouve\n");
+        return 1;
+    }
+    else
+    {
+        if(SearchToList(design, List))   //Si le mot est déjà dans la liste, c'est qu'on l'a déjà cherché
+            return trouve;
+
+        for(int j=0 ; j<towelNbr ; j++)
+        {
+            //printf("%s : %s\n", design, towel[j]);
+            if(strncmp(design, towel[j], strlen(towel[j])) == 0)
+            {
+                trouve += recur2(design+strlen(towel[j]), towelNbr, towel, List);
+            }
+        }
+
+        if(trouve == 0)
+        {
+            AddToList(design, List);
+        }
+
+        return trouve;
     }
 }
 
@@ -106,6 +202,8 @@ int main(int argc, char *argv[])
     char towel[450][11];
     char design[410][70];
     bool designPossible[450];
+    char list[1000][70];
+    uint32_t designPossibleNbr[450];
     int i = 0;
     int len = 0;
     int towelNbr = 0;
@@ -116,8 +214,15 @@ int main(int argc, char *argv[])
     memset(towel, '\0', sizeof(towel));
     memset(design, '\0', sizeof(design));
     memset(designPossible, false, sizeof(designPossible));
+    memset(list, '\0', sizeof(list));
+    memset(designPossibleNbr, 0, sizeof(designPossibleNbr));
 
 
+    /*strcpy(list[0], "string 1");
+    strcpy(list[1], "string 2");
+
+    bool ans = SearchToList("string 2", list);
+    AddToList("string 2", list);*/
 
     //Récupération des valeurs dans le fichier data
     fic = fopen("data.txt", "r");
@@ -138,7 +243,7 @@ int main(int argc, char *argv[])
                 strncpy(&towel[i][0], buf, strlen(buf));
 
             len = strlen(buf);
-            printf("longueur towel %d = %d\n", i, len);
+            //printf("longueur towel %d = %d\n", i, len);
             i++;
         }
         while(buf[len-1] == ',');
@@ -153,7 +258,7 @@ int main(int argc, char *argv[])
             fscanf(fic, "%s\n", buf);
             strcpy(&design[i][0], buf);
             len = strlen(buf);
-            printf("longueur design %d = %d\n", i, len);
+            //printf("longueur design %d = %d\n", i, len);
             i++;
         }
         while(!feof(fic));
@@ -175,7 +280,8 @@ int main(int argc, char *argv[])
     {
         //designPossible[i] = recur(design[i], strlen(design[i]), towelNbr, &towel[0], false, "\0");
         //designPossible[i] = recur(design[i], strlen(design[i]), towelNbr, &towel[0], false);
-        designPossible[i] = recur(design[i], towelNbr, &towel[0]);
+        memset(list, '\0', sizeof(list));
+        designPossible[i] = recur(design[i], towelNbr, &towel[0], list);
         printf("Design number %d, %d\n", i, designPossible[i]);
     }
 
@@ -197,18 +303,24 @@ int main(int argc, char *argv[])
     //--------------Part 2-----------------
     //Récupération des valeurs dans le fichier data
 
-
+    for(i=0 ; i<designNbr ; i++)
+    {
+        //designPossible[i] = recur(design[i], strlen(design[i]), towelNbr, &towel[0], false, "\0");
+        //designPossible[i] = recur(design[i], strlen(design[i]), towelNbr, &towel[0], false);
+        memset(list, '\0', sizeof(list));
+        //recur2(design[i], towelNbr, &towel[0], list, &designPossibleNbr[i]);
+        designPossibleNbr[i] = recur2(design[i], towelNbr, &towel[0], list);
+        printf("Design number %d, %d\n", i, designPossibleNbr[i]);
+    }
 
 
     //On ajoute toutes les solutions de chemins
-    /*for(i=0 ; i<NBR_LINES ; i++)
+    for(i=0 ; i<designNbr ; i++)
     {
-        for(j=0 ; j<NBR_COL ; j++)
-        {
-            somme2 += tab2[i][j];
-        }
+        somme2 += designPossibleNbr[i];
     }
-    printf("Somme2 = %ld\n", somme2);*/
+    printf("Somme2 = %lld\n", somme2);
+    //865 too low
 
     return 0;
 }
