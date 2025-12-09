0b16f465c2798b88989d7a37c1bc05bea558e20d
stephdaniel133 <stephdaniel133@hotmail.com>
2025-12-09 21:15:21 +0100

    2025_Jour_8

--- Noel 2025/Jour_8/main.c
+++ Noel 2025/Jour_8/main.c
@@ -3,35 +3,53 @@
 #include <string.h>
 #include <ctype.h>
 #include <stdint.h>
+#include <math.h>   //Ajouter -lm dans les options du linker
+#include <stdbool.h>
 
-//#define NBR_LINES   16
-//#define NBR_COL     15
-#define NBR_LINES   142
-#define NBR_COL     141
-
-
+//#define NBR_LINES 20
+#define NBR_LINES 1000
 
 int main(int argc, char *argv[])
 {
-    char buf1[NBR_COL+1];
+    //char buf1[NBR_COL+1];
     int64_t i = 0;
     int64_t j = 0;
     int64_t k = 0;
     int64_t somme1 = 0;
     int64_t somme2 = 0;
-    char tab1[NBR_LINES][NBR_COL];
-    int64_t tab2[NBR_LINES][NBR_COL];
-    int xS = 0;
-    int yS = 0;
+    uint64_t dmin = 0;  //Distance mini entre points
+    //uint64_t dist = 0;  //Distance entre points
+    int group = 1;
+
+    typedef struct
+    {
+        uint32_t x;
+        uint32_t y;
+        uint32_t z;
+        int group;
+        bool aff_group;
+    } point_t;
+    point_t tab1[NBR_LINES];
+
+    typedef struct
+    {
+        uint64_t d;
+        uint32_t p1;
+        uint32_t p2;
+    } dist_t;
+    dist_t dist[NBR_LINES*(NBR_LINES-1)/2];
+
+
 
 
     FILE *fic = NULL;
 
     fic = fopen("data.txt", "r");
 
-    memset(buf1, 0, sizeof(buf1));
-    memset(tab1, '1', sizeof(tab1));
-    memset(tab2, 0, sizeof(tab2));
+    //memset(buf1, 0, sizeof(buf1));
+    memset(tab1, 0, sizeof(tab1));
+    //memset(tab2, 0, sizeof(tab2));
+    memset(dist, 0, sizeof(dist));
 
 
     if(fic != NULL)
@@ -40,8 +58,7 @@ int main(int argc, char *argv[])
 
         for(i=0 ; i<NBR_LINES ; i++)
         {
-            fgets(buf1, NBR_COL+1, fic);
-            memcpy(tab1[i], buf1, NBR_COL);
+            fscanf(fic, "%d,%d,%d", &tab1[i].x, &tab1[i].y, &tab1[i].z);
             fgetc(fic); //On echappe le saut à la ligne
         }
     }
@@ -54,67 +71,97 @@ int main(int argc, char *argv[])
 
 
     //Impression du puzzle complet et recherche du 'S'
-    for(i=0 ; i<NBR_LINES ; i++)
-    {
-        for(j=0 ; j<NBR_COL ; j++)
-        {
-            printf("%c ", tab1[i][j]);
-            if(tab1[i][j] == 'S')
-            {
-                xS = i;
-                yS = j;
-            }
-        }
-        printf("\n");
-    }
-    printf("\n");
+    printf("x = %d\n", tab1[0].x);
+    printf("y = %d\n", tab1[0].y);
+    printf("z = %d\n\n", tab1[0].z);
 
-    printf("xS = %d\n", xS);
-    printf("yS = %d\n", yS);
+    printf("x = %d\n", tab1[NBR_LINES-1].x);
+    printf("y = %d\n", tab1[NBR_LINES-1].y);
+    printf("z = %d\n\n", tab1[NBR_LINES-1].z);
+
     printf("\n");
 
 
     //--------------Part 1-----------------
+    //On calcul les distances entre tous les points
     for(i=0 ; i<NBR_LINES ; i++)
     {
-        for(j=0 ; j<NBR_COL ; j++)
+        for(j=i+1 ; j<NBR_LINES ; j++)
         {
-            if(tab1[i][j] == 'S')
+            dist[k].d = abs((tab1[i].x - tab1[j].x)*(tab1[i].x - tab1[j].x) +
+                            (tab1[i].y - tab1[j].y) * (tab1[i].y - tab1[j].y) +
+                            (tab1[i].z - tab1[j].z) * (tab1[i].z - tab1[j].z));
+            dist[k].p1 = i;
+            dist[k].p2 = j;
+            printf("%4ld, %4ld, %7lu\n", i, j, dist[k].d);
+            k++;
+        }
+    }
+
+    //On trie le tableau de la plus petite à la plus grande distance entre points
+    uint64_t temp = 0;
+
+    for(i = 0; i < (sizeof(dist) / sizeof(dist[0]) - 1); i++)
+    {
+        for(j = 0; j < (sizeof(dist) / sizeof(dist[0]) - 1); j++)
+        {
+            if(dist[j].d > dist[j+1].d)  //Si la valeur de dessus est supérieure, on permutte les valeurs
             {
-                tab1[i+1][j] = '|';
-            }
-            else if((tab1[i][j] == '^') && (tab1[i-1][j] == '|'))
-            {
-                tab1[i][j-1] = '|';
-                tab1[i][j+1] = '|';
-                tab1[i+1][j-1] = '|';
-                tab1[i+1][j+1] = '|';
-                somme1++;
-            }
-            else if((tab1[i][j] == '.') && (tab1[i-1][j] == '|'))
-            {
-                tab1[i][j] = '|';
+                temp = dist[j].d;
+                dist[j].d = dist[j+1].d;
+                dist[j+1].d = temp;
+
+                temp = dist[j].p1;
+                dist[j].p1 = dist[j+1].p1;
+                dist[j+1].p1 = temp;
+
+                temp = dist[j].p2;
+                dist[j].p2 = dist[j+1].p2;
+                dist[j+1].p2 = temp;
             }
         }
     }
 
-    //Impression du puzzle complet
-    for(i=0 ; i<NBR_LINES ; i++)
+    printf("\n-----------tableau trie--------------------\n");
+    //Impression du tableau des distances
+    for(i = 0; i < sizeof(dist) / sizeof(dist[0]) - 1; i++)
     {
-        printf("%2li ", i);
-        for(j=0 ; j<NBR_COL ; j++)
-        {
-            printf("%c ", tab1[i][j]);
-        }
-        printf("\n");
+        printf("%4d, %4d, %7lu\n", dist[i].p1, dist[i].p2, dist[i].d);
     }
     printf("\n");
+
 
     printf("somme1 = %ld\n\n\n", somme1);
 
 
+
+                /*if((tab1[i].aff_group == false) || (tab1[j].aff_group == false))    //Si au moins 1 point n'a pas de groupe affecté
+            {
+                dist = abs(pow(tab1[i].x - tab1[j].x, 2) + pow(tab1[i].y - tab1[j].y, 2) + pow(tab1[i].z - tab1[j].z, 2));
+                if(dist < dmin)
+                {
+                    dmin = dist;
+                    if((tab1[i].group == 0) && (tab1[j].group == 0))  //Les 2 points trouvés n'ont pas de groupe affecté
+                    {
+                        tab1[i].group = group;
+                        tab1[j].group = group;
+                        group++;
+                    }
+                    else if((tab1[i].group != 0) && (tab1[j].group == 0))  //Le points 1 appartient déjà à un groupe, on affecte le nouveau point à ce groupe
+                    {
+                        tab1[j].group = tab1[i].group;
+                    }
+                    else if((tab1[i].group == 0) && (tab1[j].group != 0))  //Le points 2 appartient déjà à un groupe, on affecte le nouveau point à ce groupe
+                    {
+                        tab1[i].group = tab1[j].group;
+                    }
+                    i = 0;
+                    dmin = 0xFFFFFFFFFFFFFFFF;
+                    break;
+                }
+            }*/
     //--------------Part 2-----------------
-    for(i=NBR_LINES-2 ; i>1 ; i=i-2)    //On cherche les '^' depuis la base du sapin
+    /*for(i=NBR_LINES-2 ; i>1 ; i=i-2)    //On cherche les '^' depuis la base du sapin
     {
         for(j=0 ; j<NBR_COL ; j++)      //On parcours toute la ligne
         {
@@ -149,9 +196,9 @@ int main(int argc, char *argv[])
                 }
             }
         }
-    }
+    }*/
 
-    somme2 = tab2[xS+2][yS];
+    //somme2 = tab2[xS+2][yS];
 
     printf("somme2 = %ld\n", somme2);
 
